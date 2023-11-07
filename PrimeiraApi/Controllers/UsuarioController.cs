1a2fc2f6aed4b6f6f54357db00e906564c362011
CamillePB <camille.pedro.bueno@hotmail.com>
2023-11-07 14:50:16 -0300

    primeira API com .net - ainda em desenvolvimento

---
+++ PrimeiraApi/Controllers/UsuarioController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PrimeiraApi.Models;
+
+namespace PrimeiraApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<UsuarioModel>>> BuscarTodosUsuarios()
+        {
+           // List<UsuarioModel> usuarios = await _usuarioRepositorio.BuscarTodosUsuarios();
+            return Ok();
+        }
+    }
+}
---
+++ PrimeiraApi/Data/SistemaDeTarefasDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimeiraApi.Models;
+using SistemaDeTarefas.Models;
+
+namespace PrimeiraApi.Data
+{
+    //facilitador para trablhar com qualquer banco de dados
+    public class SistemaTarefasDBContex : DbContext
+    {
+        public SistemaTarefasDBContex(DbContextOptions<SistemaTarefasDBContex> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<TarefaModel> Tarefas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           // modelBuilder.ApplyConfiguration(new UsuarioMap());
+            //modelBuilder.ApplyConfiguration(new TarefaMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
